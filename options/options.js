861707735b414758f430f0b02970b5c40030adb7
Saad bin Abid <saad.abid@crowdbotics.com>
2023-10-05 00:15:28 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "module-testing-43555",
   url: "https://module-testing-43555.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- store/moduletestingAPI/api.js
+++ store/moduletestingAPI/api.js
@@ -1,58 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const moduletestingAPI = axios.create({
   baseURL: "https://module-testing-43555.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return moduletestingAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return moduletestingAPI.post(`/api/v1/login/`, payload.data)
+  return moduletestingAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return moduletestingAPI.post(`/api/v1/signup/`, payload.data)
+  return moduletestingAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return moduletestingAPI.post(`/rest-auth/login/`, payload.data)
+  return moduletestingAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return moduletestingAPI.get(`/rest-auth/logout/`)
+  return moduletestingAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return moduletestingAPI.post(`/rest-auth/logout/`)
+  return moduletestingAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return moduletestingAPI.post(`/rest-auth/password/change/`, payload.data)
+  return moduletestingAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return moduletestingAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return moduletestingAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return moduletestingAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return moduletestingAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return moduletestingAPI.post(`/rest-auth/registration/`, payload.data)
+  return moduletestingAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return moduletestingAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return moduletestingAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return moduletestingAPI.get(`/rest-auth/user/`)
+  return moduletestingAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return moduletestingAPI.put(`/rest-auth/user/`, payload.data)
+  return moduletestingAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return moduletestingAPI.patch(`/rest-auth/user/`, payload.data)
+  return moduletestingAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -68,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
